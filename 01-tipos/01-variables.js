eda0317de3fdd463ab1bc2bf512803c8639ebe2f
juakodreau <joaquindreau@gmail.com>
2024-04-09 14:22:21 -0400

    01-Variable

---
+++ 01-tipos/01-variables.js
@@ -0,0 +1,13 @@
+/*
+let nombreCompleto; <--- CamelCase
+let NombreCompleto <--- UpperCamelCase
+let nombre_completo <--- snake_case
+*/
+
+let nombre = "Hola Mundo";
+
+console.log(nombre);
+
+let apellido;
+let animal;
+apellido = "Dreau";
